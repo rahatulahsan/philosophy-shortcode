e42b7056220d0f9868d5a44514c958fbb2128c64
rahatulahsan <rahatulahsan@gmail.com>
2023-07-22 23:43:13 +0600

    First Commit

---
+++ philosophy-shortcode.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Plugin Name:       Philosophy Shortcode
+ * Plugin URI:        https://rahatulahsan.com
+ * Description:       This plugin will enable shortcode for the Philosophy Theme.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Rahatul Ahsan
+ * Author URI:        https://rahatulahsan.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://rahatulahsan.com/philosophy-shortcode
+ * Text Domain:       philosophy-shortcode
+ * Domain Path:       /languages
+ */
